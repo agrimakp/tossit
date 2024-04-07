cdaac730b1ae3eba01219d95aa073efc446da8cb
Agrima KP <qvistn111@gmail.com>
2024-04-08 09:33:01 +1000

    Initial Commit

---
+++ TossIt/TossItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossItApp.swift
+//  TossIt
+//
+//  Created by Agrima Purakkal on 8/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct TossItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
